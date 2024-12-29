3ab2d3e57e607b5b2e90c7a035e81fd49f8b6742
unknown <cristian.fernandez@datipic.com>
2024-12-29 18:34:37 +0100

    Base code

---
+++ assets/js/app.js
@@ -0,0 +1,159 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Esperar a que la página esté completamente cargada
+    window.addEventListener("load", () => {
+        // document.documentElement.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';
+
+        const preloader = document.getElementById("preloader");
+        const mainContent = document.getElementById("main-content");
+        
+        setTimeout(() => {
+            preloader.classList.add("hidden");
+            mainContent.classList.add("visible");
+        }, 500);
+
+        AOS.init({
+            duration: 2000,
+        });
+
+    });
+});
+
+  
+
+
+const util = (() => {
+
+    const animation = () => {
+        const duration = 15 * 1000;
+        const animationEnd = Date.now() + duration;
+        const colors = ["#FFC0CB", "#FF1493", "#C71585"];
+
+        const randomInRange = (min, max) => {
+            return Math.random() * (max - min) + min;
+        };
+
+        const heart = confetti.shapeFromPath({
+            path: 'M167 72c19,-38 37,-56 75,-56 42,0 76,33 76,75 0,76 -76,151 -151,227 -76,-76 -151,-151 -151,-227 0,-42 33,-75 75,-75 38,0 57,18 76,56z',
+            matrix: [0.03333333333333333, 0, 0, 0.03333333333333333, -5.566666666666666, -5.533333333333333]
+        });
+
+        (function frame() {
+            const timeLeft = animationEnd - Date.now();
+
+            colors.forEach((color) => {
+                confetti({
+                    particleCount: 1,
+                    startVelocity: 0,
+                    ticks: Math.max(50, 100 * (timeLeft / duration)),
+                    origin: {
+                        x: Math.random(),
+                        y: Math.abs(Math.random() - (timeLeft / duration)),
+                    },
+                    zIndex: 1057,
+                    colors: [color],
+                    shapes: [heart],
+                    drift: randomInRange(-0.5, 0.5),
+                    gravity: randomInRange(0.5, 1),
+                    scalar: randomInRange(0.5, 1),
+                });
+            });
+
+            if (timeLeft > 0) {
+                requestAnimationFrame(frame);
+            }
+        })();
+    };
+    
+    const init = async (button) => {
+        button.disabled = true;
+        // document.documentElement.style.overflow = 'auto';
+        document.body.style.overflow = 'auto';  // o 'scroll'
+        // document.documentElement.style.overflowX = 'hidden';
+        document.body.style.overflowX = 'hidden';
+        const mainContent = document.getElementById("main-content");
+        const pageContent = document.getElementById("page-content");
+        mainContent.classList.remove("visible");
+        pageContent.classList.add("visible");
+
+        audio.play();
+        document.getElementById('btn-music').style.display = 'block';
+        // timer();
+        
+        confetti({
+            origin: { y: 0.9 },
+            zIndex: 1057
+        });
+        // await session.check();
+        animation();
+        AOS.refreshHard();
+    };
+
+    const timer = () => {
+        let countDownDate = (new Date(document.getElementById('tampilan-waktu').getAttribute('data-waktu').replace(' ', 'T'))).getTime();
+
+        setInterval(() => {
+            let distance = Math.abs(countDownDate - (new Date()).getTime());
+
+            document.getElementById('hari').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+            document.getElementById('jam').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            document.getElementById('menit').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            document.getElementById('detik').innerText = Math.floor((distance % (1000 * 60)) / 1000);
+        }, 1000);
+    };
+
+    const music = (btn) => {
+        if (btn.getAttribute('data-status') !== 'true') {
+            btn.setAttribute('data-status', 'true');
+            audio.play();
+            btn.innerHTML = '<i class="fa-solid fa-circle-pause spin-button"></i>';
+        } else {
+            btn.setAttribute('data-status', 'false');
+            audio.pause();
+            btn.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
+        }
+    };
+
+    const animate = (svg, timeout, classes) => {
+        let handler = null;
+
+        handler = setTimeout(() => {
+            svg.classList.add(classes);
+            handler = null;
+        }, timeout);
+    };
+
+    return {
+        init,
+        // modal,
+        music,
+        // salin,
+        // escapeHtml,
+        // show,
+        animate
+    };
+})();
+
+const audio = (() => {
+    let audio = null;
+
+    const singleton = () => {
+        if (!audio) {
+            audio = new Audio();
+            audio.src = document.getElementById('btn-music').getAttribute('data-url');
+            audio.load();
+            audio.currentTime = 0;
+            audio.autoplay = true;
+            audio.muted = false;
+            audio.loop = true;
+            audio.volume = 1;
+        }
+
+        return audio;
+    };
+
+    return {
+        play: () => singleton().play(),
+        pause: () => singleton().pause(),
+    };
+})();
