f89b83bf9c61963bce318e3ea8bc738f899d48ac
dpcfern <cristian.fernandez@datipic.com>
2025-01-18 19:49:28 +0100

    Send Data Form

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,22 +47,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         // Enviar Formulario
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbykYagF_y6QvrKjDRvyxfys3RmAwxZGySd_FZ_EjhN1fC-TbrVeYNgo-JqXPWThDdLI6A/exec';
+        const scriptURL = "https://script.google.com/macros/s/AKfycby5YMw7iSaNq5JkwMX8BJnuphmfTxq8KzXcaLg1CKj0lhJ_ixTxearEvLYAZHleLsubWw/exec"
 
-        document.getElementById('confirm-form').addEventListener('submit', async (e) => {
+        const form = document.forms['confirm-form'];
+        form.addEventListener('submit', async (e) => {
             e.preventDefault();
 
-            const formData = new FormData(e.target);
-            const data = {};
-            formData.forEach((value, key) => data[key] = value);
-            console.log(data);
+            const formData = new FormData(form);
             try {
                 const response = await fetch(scriptURL, {
                     method: 'POST',
-                    body: JSON.stringify(data),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
+                    body: formData,
                 });
                 alert('Formulario enviado exitosamente.');
             } catch (error) {
